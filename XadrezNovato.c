512ef158e66a15b2a98c9498ff99890614334017
Edinan60 <116977688+Edinan60@users.noreply.github.com>
2025-03-22 20:21:09 +0000

    XadrezNovato

--- XadrezNovato.c
+++ XadrezNovato.c
@@ -2,6 +2,7 @@
  
 int main() {
 
+    //movimento do bispo 5 casas para a Direita em While
 int torre = 1, bisbo = 1, rainha = 1;
     
 while (torre <= 5)
@@ -9,11 +10,12 @@ while (torre <= 5)
     printf("Torre moveu-se para Direita.\n", torre);
     torre++;
 }
-do
+do //movimento do bispo 5 casas na diagonal(Cima, Direita) em Do-While
 {
     printf("Bisbo moveu-se para Cima, Direita.\n", bisbo);
     bisbo++;
 } while (bisbo <= 5);
+//movimento da rainha 8 casas para a Esquerda em For
 for (int rainha = 1; rainha <= 8; rainha++)
 {
     printf("Rainha moveu-se para esquerda.\n", rainha);
